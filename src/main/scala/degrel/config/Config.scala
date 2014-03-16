9528d68782e91c4cf58b7364c128ce37f6dd0816
kanosaki <kanosaki@gmail.com>
2014-03-16 12:25:05 +0900

    wip

--- src/main/scala/degrel/config/Config.scala
+++ src/main/scala/degrel/config/Config.scala
@@ -1,20 +1,5 @@
 package degrel.config
 
-trait Config extends Serializable {
-  def parent: Config
+class Config {
 
-  def get(key: Symbol): List[Any]
-
-  /**
-   * Resolve order: CLIArguments -> this -> parent -> parent.parent -> ... -> RootConfig
-   * @param key
-   * @return
-   */
-  def resolve(key: Symbol) = {
-    CLIArguments.get(key) ++ parent.resolveTree(key)
-  }
-
-  protected def resolveTree(key: Symbol): List[Any] = {
-    this.get(key) ++ parent.resolveTree(key)
-  }
 }
