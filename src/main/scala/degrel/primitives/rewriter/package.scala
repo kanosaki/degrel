2b336cdb39aadca875f624667cecb2e29a2786e4
kanosaki <kanosaki@gmail.com>
2015-05-13 01:49:08 +0900

    Intに対する比較演算を実装しました

--- src/main/scala/degrel/primitives/rewriter/package.scala
+++ src/main/scala/degrel/primitives/rewriter/package.scala
@@ -1,9 +1,9 @@
 package degrel.primitives
 
 import degrel.primitives.rewriter.cell.{Fin, SendMessage}
-import degrel.primitives.rewriter.lang.If
+import degrel.primitives.rewriter.lang.{IntegerPredicate, If}
 import degrel.primitives.rewriter.math.Plus
 
 package object rewriter {
-  val default = Seq(new Plus(), new SendMessage(), new Fin(), new If())
+  val default = Seq(new Plus(), new SendMessage(), new Fin(), new If()) ++ IntegerPredicate.all
 }
