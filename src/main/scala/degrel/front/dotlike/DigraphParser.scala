1c36991d39a6ff567d3f89987f2572b2adb76872
kanosaki <kanosaki@gmail.com>
2015-06-06 00:26:12 +0900

    ditto

--- src/main/scala/degrel/front/dotlike/DigraphParser.scala
+++ src/main/scala/degrel/front/dotlike/DigraphParser.scala
@@ -21,7 +21,7 @@ object DigraphParser extends JavaTokenParsers {
   val PAT_ATTR_KEY = """[^:]+""".r
   val IDENTIFIER_SEP = "$"
 
-  def eol = sys.props("line.separator")
+  def eol = "\\r\\n?".r
 
   def eoi = """\z""".r // End of Input
 
