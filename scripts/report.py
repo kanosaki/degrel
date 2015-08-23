3bb13f2418d2cfca37845fba0aef4ba239fce681
kanosaki <kanosaki@gmail.com>
2015-08-23 17:20:18 +0900

    グラフを描画するときに，X軸のデータを指定できるようにしました

--- scripts/report.py
+++ scripts/report.py
@@ -9,6 +9,7 @@ import numpy as np
 
 JVM_WARM = 10
 
+
 class ParamJson(object):
     def __init__(self, fpath):
         self.path = fpath
@@ -32,27 +33,65 @@ class ParamJson(object):
     def bench_config(self, name):
         return [bc for bc in self.obj['bench_list'] if bc['name'] == name][0]
 
-    def create_report(self, columns):
-        for (col, name) in itertools.product(columns, self.names):
-            tasks = [t for t in self.tasks if t.name == name]
-            legends = set(t.legend for t in tasks)
-            bench_config = self.bench_config(name)
-            title = '%s-%s' % (col, name)
-            plt.clf()
-            plt.title(title)
-            ymax = 0
-            for legend in legends:
-                ys_all = np.array([
-                    y.col(col) for y in tasks if y.legend == legend])
-                y = np.average(ys_all, axis=0)  # average each samples
+    def _create_report_index(self, col, name):
+        tasks = [t for t in self.tasks if t.name == name]
+        legends = set(t.legend for t in tasks)
+        bench_config = self.bench_config(name)
+        title = '%s-%s-index' % (name, col)
+        plt.clf()
+        plt.title(title)
+        ymax = 0
+        for legend in legends:
+            ys_all = np.array([
+                y.col(col) for y in tasks if y.legend == legend])
+            y = np.average(ys_all, axis=0)  # average each samples
+            x = np.arange(len(y))
+            plt.xlabel('index')
+            plt.ylabel(col)
+            plt.plot(x, y, label=legend)
+            if ymax < max(y):
+                ymax = max(y)
+        plt.vlines([bench_config['warm_up']], 0,
+                    ymax, linestyles="dashed")
+        plt.legend()
+        self.savefig(title)
+
+    def _create_report(self, col, name, row):
+        tasks = [t for t in self.tasks if t.name == name]
+        legends = set(t.legend for t in tasks)
+        title = '%s-%s-%s' % (name, col, row)
+        plt.clf()
+        plt.title(title)
+        ymax = 0
+        for legend in legends:
+            ys_all = np.array([
+                y.col(col) for y in tasks if y.legend == legend
+            ])
+            y = np.average(ys_all, axis=0)  # average each samples
+            if row == 'index':
                 x = np.arange(len(y))
-                plt.plot(x, y, label=legend)
-                if ymax < max(y):
-                    ymax = max(y)
+            else:
+                x = [x for x in tasks if x.legend == legend][0].col(row)
+            plt.xlabel(row)
+            plt.ylabel(col)
+            plt.plot(x, y, label=legend)
+            if ymax < max(y):
+                ymax = max(y)
+        if row == 'index':
+            bench_config = self.bench_config(name)
             plt.vlines([bench_config['warm_up']], 0,
-                       ymax, linestyles="dashed")
-            plt.legend()
-            self.savefig(title)
+                        ymax, linestyles="dashed")
+        plt.legend()
+        self.savefig(title)
+
+    def create_reports(self, y_axises, x_axises=None):
+        x_axises = x_axises or ['index']
+        for (y_ax, name, x_ax) in itertools.product(
+            y_axises,
+            self.names,
+            x_axises):
+            if y_ax != x_ax:
+                self._create_report(y_ax, name, x_ax)
 
 
 class BenchTask(object):
@@ -78,31 +117,12 @@ class BenchTask(object):
         return [r[colname] for r in self.reports]
 
 
-class ResultJson(object):
-    def __init__(self, fpath):
-        self.result = json.load(open(fpath))
-        self.reports = self.result['reports']
-        self.rps = np.array([i['rps'] for i in self.reports])
-        self.initial_sizes = np.array([i['initialMainSize'] for i in self.reports])
-        self.x = np.arange(0, len(self.reports), 1)
-
-    def plot_size(self):
-        plt.clf()
-        y = self.initial_sizes
-        plt.title("Graph size")
-        plt.vlines([JVM_WARM], 0, max(y), linestyles="dashed")
-        plt.plot(self.x, y, label='size')
-
-    def plot_rps(self):
-        plt.clf()
-        y = self.rps
-        plt.title("Rewrite per second")
-        plt.vlines([JVM_WARM], 0, max(y), linestyles="dashed")
-        plt.plot(self.x, y, label='rps')
-
 def process(param_path):
     param = ParamJson(param_path)
-    param.create_report(['rps', 'initialMainSize', 'rewriteTryCount'])
+    param.create_reports(
+        ['rps', 'initialMainSize', 'rewriteTryCount'],
+        ['index', 'initialMainSize']
+    )
 
 
 if __name__ == '__main__':
